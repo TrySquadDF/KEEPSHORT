3f7dec309401b4a88ded66d35f6b0e0e828c23a5
TrySquadDF <sjpdkedkdjkdkdjdmddd@gmail.com>
2022-11-30 02:52:41 +0300

    Git: update.

--- vite.config.ts
+++ vite.config.ts
@@ -7,10 +7,10 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/KEEPSHORT/",
   test: {
     globals: true,
     environment: "jsdom",
     setupFiles: "./src/test/setup.ts",
   },
-  base: "/KEEPSHORT/",
 });
